4332205180626315aba47cc143c397763dcc0b93
Akash_96 <realakash9630@gmail.com>
2022-11-18 10:14:53 +0530

    alter

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="image-container">
+      <img
+        src="./Fita-certificate.webp"
+        alt="FITA Academy certificate"
+        style={{ width: "700px", height: "900px" }}
+      />
+    </div>
+  );
+}
+
+export default App;
